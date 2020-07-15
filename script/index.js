8d256f39c4062192ef50f58cb6f20392da831ed4
Byungchan-Park <pbc9236@gmail.com>
2020-07-15 18:22:26 +0900

    user-interaction complete about header

---
+++ script/index.js
@@ -0,0 +1,40 @@
+'use strict'
+
+let header = document.getElementById('header')
+let navbar = document.getElementById('gnb')
+
+header.addEventListener('mouseover', () => {
+  header.classList.add('on')
+})
+header.addEventListener('mouseout', () => {
+  header.classList.remove('on')
+})
+
+let last_known_scroll_position = 0
+let ticking = false
+
+function doSomething(scroll_pos) {
+  scroll_pos !== 0 ? header.classList.add('on') : header.classList.remove('on')
+}
+
+window.addEventListener('scroll', function (e) {
+  last_known_scroll_position = window.scrollY
+
+  if (!ticking) {
+    window.requestAnimationFrame(function () {
+      doSomething(last_known_scroll_position)
+      ticking = false
+    })
+
+    ticking = true
+  }
+})
+
+navbar.addEventListener('mouseover', () => {
+  navbar.classList.add('open')
+  header.classList.add('open')
+})
+navbar.addEventListener('mouseout', () => {
+  navbar.classList.remove('open')
+  header.classList.remove('open')
+})
